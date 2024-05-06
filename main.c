b9c896ad8c04462c9f92e4b72d372324249e6e7f
edaywalid <becharwalid9@gmail.com>
2024-05-06 03:22:36 +0100

    allow the user to modify the controllers' positions

--- main.c
+++ main.c
@@ -50,8 +50,9 @@ void render_bezier_markers(Vector2 a , Vector2 b ,
     for(float p = 0.0f ; p <= 1.0f ; p+= s){
         Vector2 ab = lerpv2(a, b, p);
         Vector2 bc = lerpv2(b, c, p);
+        Vector2 cd = lerpv2(c, d, p);
         Vector2 abc = lerpv2(ab, bc, p);
-        Vector2 bcd  = lerpv2(bc, bc, p);
+        Vector2 bcd  = lerpv2(bc, cd, p);
         Vector2 abcd = lerpv2(abc, bcd, p);
         render_marker(abcd, PURPLE);
     }
@@ -61,6 +62,23 @@ void render_bezier_markers(Vector2 a , Vector2 b ,
 #define PS_CAPACITY 256
 Vector2 ps[PS_CAPACITY];
 size_t ps_count = 0;
+int ps_selected = -1;
+
+// ps at 
+int ps_at(Vector2 pos){
+    const Vector2 ps_size = vec2_constr(MARKER_SIZE, MARKER_SIZE);
+    for(size_t i = 0 ; i < ps_count ; i++){
+        const Vector2 ps_begin = vec2_sub(ps[i], vec2_scale(ps_size, 0.5f)) ;
+        const Vector2 ps_end = vec2_add(ps_begin , ps_size);
+        if(ps_begin.x   <= pos.x && pos.x <= ps_end.x
+           && ps_begin.y<= pos.y && pos.y <= ps_end.y
+        )
+        {   
+            return (int) i ;
+        }
+    }
+    return -1 ;
+}
 
 // main funciton 
 int main() {
@@ -71,7 +89,7 @@ int main() {
 
     Vector2 begin = {100.0f, 100.0f};
     Vector2 end = { 250.0f, 400.0f };
-        
+    int j = 0; 
     SetTargetFPS(SCREEN_FPS);
     float time = 0.0f;
     while (!WindowShouldClose()) {
@@ -79,14 +97,25 @@ int main() {
         ClearBackground(DARKGRAY);
         if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
             Vector2 mouse_pos = GetMousePosition();
-            ps[ps_count++] = vec2_constr(mouse_pos.x, mouse_pos.y);
+            if(ps_count<4){
+                ps[ps_count++] = vec2_constr(mouse_pos.x, mouse_pos.y);
+            }else {
+                ps_selected = ps_at(mouse_pos);
+                printf("ps_selected : %d\n" , ps_selected);
+            }
+        }
+        else if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+            Vector2 mouse_pos = GetMousePosition();
+            if(ps_selected != -1){
+               ps[ps_selected] = mouse_pos; 
+            }
         }
         const float p =  (sinf(time) + 1.0f) * 0.5f ; 
         for(size_t i = 0 ; i < ps_count ; i++){
             render_marker(ps[i], RED);
         }
         if (ps_count >= 4) {
-            render_bezier_markers(ps[0],ps[1],ps[2],ps[3] ,0.1f);
+            render_bezier_markers(ps[0],ps[1],ps[2],ps[3] ,0.05f);
         }
         EndDrawing(); 
         time += DELAY_SEC;
